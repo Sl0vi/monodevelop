Sl0vi/monodevelop

commit 176ab0480dd24fa2e3623ea5b85c228dd4036ab1

Fixed 'Bug 32046 - Janky animations on tooltips '

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/TooltipInformationWindow.cs
@@ -281,10 +281,19 @@ namespace MonoDevelop.Ide.CodeCompletion
 			vb2.PackStart (hb, true, true, 0);
 			ContentBox.Add (vb2);
 
+			vb2.ShowAll ();
 			SetDefaultScheme ();
+		}
 
-			ShowAll ();
-			DesktopService.RemoveWindowShadow (this);
+		public override void RepositionWindow(Gdk.Rectangle? newCaret = null)
+		{
+			// Setting the opicity delayed to 1 is a hack to ensure smooth animation popup see "Bug 32046 - Janky animations on tooltips"
+			Opacity = 0;
+			base.RepositionWindow(newCaret);
+			GLib.Timeout.Add (50, delegate {
+				Opacity = 1;
+				return false;
+			});
 		}
 	}
 }

main/src/core/MonoDevelop.Ide/MonoDevelop.Components.MainToolbar/SearchPopupWindow.cs
@@ -655,11 +655,12 @@ namespace MonoDevelop.Components.MainToolbar
 			}
 			if (tooltip == null || string.IsNullOrEmpty (tooltip.SignatureMarkup) || token.IsCancellationRequested)
 				return;
-			
-			declarationviewwindow.Clear ();
-			declarationviewwindow.AddOverload (tooltip);
-			declarationviewwindow.CurrentOverload = 0;
-			declarationViewTimer = GLib.Timeout.Add (250, DelayedTooltipShow);
+			Application.Invoke (delegate {
+				declarationviewwindow.Clear ();
+				declarationviewwindow.AddOverload (tooltip);
+				declarationviewwindow.CurrentOverload = 0;
+				declarationViewTimer = GLib.Timeout.Add (250, DelayedTooltipShow);
+			});
 		}
 
 		bool DelayedTooltipShow ()

main/src/core/MonoDevelop.Ide/MonoDevelop.Components/PopoverWindow.cs
@@ -198,7 +198,7 @@ namespace MonoDevelop.Components
 			set;
 		}
 
-		public void RepositionWindow (Gdk.Rectangle? newCaret = null)
+		public virtual void RepositionWindow (Gdk.Rectangle? newCaret = null)
 		{
 			if (parent == null)
 				return;

commit b6b568d4a02cf79ce6b65ab5bd0444713f6370b2

[Ide]Stop tooltip from appearing after completion list has closed
Commit 176ab0480dd24fa2e3623ea5b85c228dd4036ab1 introduced a bug where the
tooltip window would appear and stay open after the completion list was closed.

There is a chance that the hack using a timeout to change opacity gets run
after the tooltip window should have been closed. This can lead to a timing
issue, where the tooltip window appears and stays open  after the completion
list has closed.

Checking if the parent is visible before changing the opacity stops the tooltip
window from appearing when it shouldn't.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/CompletionListWindow.cs
@@ -903,8 +903,7 @@ namespace MonoDevelop.Ide.CodeCompletion
 			if (data != currentData)
 				HideDeclarationView ();
 
-			//declarationViewTimer = GLib.Timeout.Add (150, DelayedTooltipShow);
-			DelayedTooltipShowAsync (); // Fix async tooltip bug
+			declarationViewTimer = GLib.Timeout.Add (150, DelayedTooltipShow);
 		}
 		
 		void HideDeclarationView ()

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/TooltipInformationWindow.cs
@@ -314,7 +314,8 @@ namespace MonoDevelop.Ide.CodeCompletion
 			Opacity = 0;
 			base.RepositionWindow(newCaret);
 			GLib.Timeout.Add (50, delegate {
-				Opacity = 1;
+				if (Parent.Visible)
+					Opacity = 1;
 				return false;
 			});
 		}